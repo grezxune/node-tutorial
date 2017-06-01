b91bc797998a7f4011289dab664f0a87141bdfc1
Trzebiatowski <trzebiat@mfldclin.org>
2017-06-01 16:51:26 -0500

    Added GET /todos route

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -24,6 +24,16 @@ app.post('/todos', (req, res) => {
 });
 
 app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({
+      todos: todos
+    });
+  }, (err) => {
+    res.status(400).send(err);
+  });
+});
+
+app.get('/todos', (req, res) => {
 });
 
 app.listen(port, () => {
